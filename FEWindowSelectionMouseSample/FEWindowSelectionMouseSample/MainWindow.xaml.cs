b97ce0f31ca369fe51520b4402a1d09dafc3ced2
FreeEcho <20364419+FreeEcho1@users.noreply.github.com>
2022-06-26 08:50:13 +0900

    Add files via upload

--- FEWindowSelectionMouseSample/FEWindowSelectionMouseSample/MainWindow.xaml.cs
+++ FEWindowSelectionMouseSample/FEWindowSelectionMouseSample/MainWindow.xaml.cs
@@ -1,62 +1,55 @@
-﻿namespace FEWindowSelectionMouseSample
+﻿namespace FEWindowSelectionMouseSample;
+
+public partial class MainWindow : System.Windows.Window
 {
-    public partial class MainWindow : System.Windows.Window
+    [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true, CharSet = System.Runtime.InteropServices.CharSet.Unicode)]
+    private static extern int GetWindowTextLength(System.IntPtr hWnd);
+    [System.Runtime.InteropServices.DllImport("user32.dll", CharSet = System.Runtime.InteropServices.CharSet.Unicode, SetLastError = true)]
+    private static extern int GetWindowText(System.IntPtr hWnd, System.Text.StringBuilder lpString, int nMaxCount);
+
+    FreeEcho.FEWindowSelectionMouse.WindowSelectionFrame WindowSelectionFrame;
+
+    public MainWindow()
     {
-        [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true, CharSet = System.Runtime.InteropServices.CharSet.Auto)]
-        private static extern int GetWindowTextLength(System.IntPtr hWnd);
-        [System.Runtime.InteropServices.DllImport("user32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto, SetLastError = true)]
-        private static extern int GetWindowText(System.IntPtr hWnd, System.Text.StringBuilder lpString, int nMaxCount);
+        InitializeComponent();
 
-        FreeEcho.FEWindowSelectionMouse.WindowSelectionFrame WindowSelectionFrame;
+        WindowSelectionLeftButtonUpButton.PreviewMouseDown += WindowSelectionLeftButtonUpButton_PreviewMouseDown;
+    }
 
-        public MainWindow()
+    private void WindowSelectionLeftButtonUpButton_PreviewMouseDown(
+        object sender,
+        System.Windows.Input.MouseButtonEventArgs e
+        )
+    {
+        try
         {
-            InitializeComponent();
-
-            try
+            WindowSelectionFrame = new()
             {
-                WindowSelectionLeftButtonUpButton.PreviewMouseDown += WindowSelectionLeftButtonUpButton_PreviewMouseDown;
-            }
-            catch
-            {
-            }
+                MouseLeftUpStop = true
+            };
+            WindowSelectionFrame.MouseLeftButtonUp += WindowSelectionFrame_MouseLeftButtonUpEvent;
+            WindowSelectionFrame.StartWindowSelection();
         }
-
-        private void WindowSelectionLeftButtonUpButton_PreviewMouseDown(
-            object sender,
-            System.Windows.Input.MouseButtonEventArgs e
-            )
+        catch
         {
-            try
-            {
-                WindowSelectionFrame = new()
-                {
-                    MouseLeftUpStop = true
-                };
-                WindowSelectionFrame.MouseLeftButtonUp += WindowSelectionFrame_MouseLeftButtonUpEvent;
-                WindowSelectionFrame.StartWindowSelection();
-            }
-            catch
-            {
-            }
         }
+    }
 
-        private void WindowSelectionFrame_MouseLeftButtonUpEvent(
-            object sender,
-            FreeEcho.FEWindowSelectionMouse.MouseLeftButtonUpEventArgs e
-            )
+    private void WindowSelectionFrame_MouseLeftButtonUpEvent(
+        object sender,
+        FreeEcho.FEWindowSelectionMouse.MouseLeftButtonUpEventArgs e
+        )
+    {
+        try
         {
-            try
-            {
-                System.Text.StringBuilder stringData = new(GetWindowTextLength(WindowSelectionFrame.SelectedHwnd) + 1);
-                GetWindowText(WindowSelectionFrame.SelectedHwnd, stringData, stringData.Capacity);
-                System.Diagnostics.Debug.WriteLine(stringData.ToString());
+            System.Text.StringBuilder stringData = new(GetWindowTextLength(WindowSelectionFrame.SelectedHwnd) + 1);
+            _ = GetWindowText(WindowSelectionFrame.SelectedHwnd, stringData, stringData.Capacity);
+            System.Diagnostics.Debug.WriteLine(stringData.ToString());
 
-                WindowSelectionFrame = null;
-            }
-            catch
-            {
-            }
+            WindowSelectionFrame = null;
+        }
+        catch
+        {
         }
     }
 }
